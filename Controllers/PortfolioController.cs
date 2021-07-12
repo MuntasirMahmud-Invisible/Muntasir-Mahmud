86aa36c43ff513a07de641eb65bc4cb7f334f16a
Muntasir Mahmud <muntasirmahmuddhruba@gmail.com>
2021-07-13 00:09:27 +0600

    Add project files.

---
+++ Controllers/PortfolioController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace MuntasirPortfolio.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        // GET: Portfolio
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PortfolioDetail()
+        {
+            return View();
+        }
+
+    }
+
+}
